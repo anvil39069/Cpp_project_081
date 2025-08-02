dbf345c45e38ae051f5894098e4d62c3e4d9af34
anvil39069 <anvilsky123tg@gmail.com>
2025-08-02 15:29:57 +0545

    testing by putting main file

---
+++ maain.cpp
@@ -0,0 +1,20 @@
+#include<raylib.h>
+#include<iostream>
+using namespace std;
+
+int main(){
+     InitWindow(300,600,"Tetris");
+     SetTargetFPS(60);
+     while(WindowShouldClose()==false)
+     {
+        BeginDrawing();
+        
+        
+        EndDrawing();
+
+     }
+
+
+
+     CloseWindow();
+}
