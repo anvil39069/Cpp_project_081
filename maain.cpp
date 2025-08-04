4de6dbdaa69925ed22a21ebfa70022be94f78f6a
anvil39069 <anvilsky123tg@gmail.com>
2025-08-04 22:32:35 +0545

    Added colors for now

--- maain.cpp
+++ maain.cpp
@@ -1,10 +1,17 @@
 #include<raylib.h>
 #include<iostream>
+#include "grid.h"
 using namespace std;
 
 int main(){
+    Color darkblue={44,44,127,255};
      InitWindow(300,600,"Tetris");
      SetTargetFPS(60);
+
+     Grid grid =Grid();
+     grid.print();
+
+     
      while(WindowShouldClose()==false)
      {
         BeginDrawing();
